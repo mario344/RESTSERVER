906e97dfcd06d20de308b964b21d378af0e832b2
mario344 <you@example.com>
2020-10-07 12:34:23 -0500

    variable de entonorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb+srv://nosferatum963:Yzb4aqKJyKqpNwK1@cluster0.nhnhj.mongodb.net/cafe'
+    urlBD = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlBD;
